c83371769730848e58b21702c80cc58299d1bc0e
arsenyan.d <arsenyan.d@office.lan>
2023-12-19 20:19:18 +0400

    add project

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,58 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <header>
+      <img src="@/assets/Masrik.png" alt="">
+      <div style="display: flex; flex-direction: column; align-items: start">
+        <p><b>Դասարան `</b> 7/ա</p>
+        <p><b>ՈՒսուցչուհուի `</b> Սիրան Դավթյան</p>
+      </div>
+    </header>
+    <CreditCalculator />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import CreditCalculator from '@/components/CreditCalculator.vue'
 
 export default {
-  name: 'HomeView',
+  name: 'App',
   components: {
-    HelloWorld
+    CreditCalculator
   }
 }
 </script>
+
+<style>
+header {
+  position: fixed;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  column-gap: 20px;
+  top: 0;
+  width: 100%;
+  background-color: #333;
+  color: white;
+  text-align: center;
+  padding: 10px 0;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+  z-index: 1000;
+
+  img  {
+    width: 60px;
+    height: 60px;
+  }
+}
+* {
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 90px;
+}
+</style>
